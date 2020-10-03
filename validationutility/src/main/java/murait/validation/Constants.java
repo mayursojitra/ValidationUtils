dfe9cbb1a712579a41d072d1c2ed663ab05c405b
Pratik Butani <pratik13butani@gmail.com>
2020-10-03 12:58:06 +0530

    Upgraded to AndroidX and Android 10

--- validationutility/src/main/java/murait/validation/Constants.java
+++ validationutility/src/main/java/murait/validation/Constants.java
@@ -2,7 +2,7 @@ package murait.validation;
 
 class Constants {
     static final String emailPattern = "([\\w\\-]([\\.\\w])+[\\w]+@([\\w\\-]+\\.)+[A-Za-z]{2,4})";
-    static final String phonePatterns[] = {"\\d{10}",
+    static final String[] phonePatterns = {"\\d{10}",
             "\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}",
             "\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}",
             "\\(\\d{3}\\)-\\d{3}-\\d{4}"};
